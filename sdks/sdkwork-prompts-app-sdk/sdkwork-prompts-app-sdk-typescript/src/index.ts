f6f67fd95369443f2ae10ed8bda7ffb569319dd3
zowork <sdkwork@zowalk.com>
2026-07-20 01:30:22 +0800

    chore: sync local changes

--- sdks/sdkwork-prompts-app-sdk/sdkwork-prompts-app-sdk-typescript/src/index.ts
+++ sdks/sdkwork-prompts-app-sdk/sdkwork-prompts-app-sdk-typescript/src/index.ts
@@ -1,16 +1,16 @@
 import {
   createClient as createGeneratedAppClient,
-  SdkworkAppClient,
-} from '../generated/server-openapi/src/index';
-import type { SdkworkAppConfig } from '../generated/server-openapi/src/types/common';
+  SdkworkPromptsAppClient,
+} from '../../generated/server-openapi/src/index';
+import type { SdkworkAppConfig } from '../../generated/server-openapi/src/types/common';
 
-export { SdkworkAppClient, createGeneratedAppClient };
+export { SdkworkPromptsAppClient, createGeneratedAppClient };
 export type { SdkworkAppConfig };
-export * from '../generated/server-openapi/src/types';
-export * from '../generated/server-openapi/src/api';
-export * from '../generated/server-openapi/src/http';
-export * from '../generated/server-openapi/src/auth';
+export * from '../../generated/server-openapi/src/types';
+export * from '../../generated/server-openapi/src/api';
+export * from '../../generated/server-openapi/src/http';
+export * from '../../generated/server-openapi/src/auth';
 
-export function createClient(config: SdkworkAppConfig): SdkworkAppClient {
+export function createClient(config: SdkworkAppConfig): SdkworkPromptsAppClient {
   return createGeneratedAppClient(config);
 }
