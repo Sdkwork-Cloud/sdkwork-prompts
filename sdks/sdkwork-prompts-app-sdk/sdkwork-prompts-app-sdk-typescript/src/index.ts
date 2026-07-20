07cbe9a9bcc102045c65378cc6ff815364f79d4f
zowork <sdkwork@zowalk.com>
2026-07-21 01:18:35 +0800

    chore: workspace sync commit

--- sdks/sdkwork-prompts-app-sdk/sdkwork-prompts-app-sdk-typescript/src/index.ts
+++ sdks/sdkwork-prompts-app-sdk/sdkwork-prompts-app-sdk-typescript/src/index.ts
@@ -1,15 +1,12 @@
 import {
   createClient as createGeneratedAppClient,
   SdkworkPromptsAppClient,
-} from '../../generated/server-openapi/src/index';
-import type { SdkworkAppConfig } from '../../generated/server-openapi/src/types/common';
+  type SdkworkAppConfig,
+} from 'sdkwork-prompts-app-sdk-generated-typescript';
 
 export { SdkworkPromptsAppClient, createGeneratedAppClient };
 export type { SdkworkAppConfig };
-export * from '../../generated/server-openapi/src/types';
-export * from '../../generated/server-openapi/src/api';
-export * from '../../generated/server-openapi/src/http';
-export * from '../../generated/server-openapi/src/auth';
+export * from 'sdkwork-prompts-app-sdk-generated-typescript';
 
 export function createClient(config: SdkworkAppConfig): SdkworkPromptsAppClient {
   return createGeneratedAppClient(config);
